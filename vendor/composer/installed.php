038a6306e7cae0ab6d1cbf98c8b83f648f887a29
JDavid046 <jdibarraochoa@hotmail.com>
2021-07-25 19:11:30 -0500

    Instalación de Bootstrap

---
+++ vendor/composer/installed.php
@@ -0,0 +1,40 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '23f11dfa1bc6e97cdfa01d095cf7db11ffbba42b',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '23f11dfa1bc6e97cdfa01d095cf7db11ffbba42b',
+    ),
+    'twbs/bootstrap' => 
+    array (
+      'pretty_version' => 'v5.0.2',
+      'version' => '5.0.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '688bce4fa695cc360a0d084e34f029b0c192b223',
+    ),
+    'twitter/bootstrap' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v5.0.2',
+      ),
+    ),
+  ),
+);
