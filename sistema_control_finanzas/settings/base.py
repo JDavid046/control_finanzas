808fea27f666c03682961946fa413b20acf4a187
JDavid046 <jdibarraochoa@hotmail.com>
2021-07-29 16:19:11 -0500

    Arreglo makemigrations

--- sistema_control_finanzas/settings/base.py
+++ sistema_control_finanzas/settings/base.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'sistema_control/static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'sistema_control/static')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
