a952ee4bea480f3ec0bd733b947b7371c147d948
JDavid046 <jdibarraochoa@hotmail.com>
2021-07-29 11:23:57 -0500

    Redirecciónn static folders

--- sistema_control_finanzas/settings/base.py
+++ sistema_control_finanzas/settings/base.py
@@ -18,12 +18,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'sistema_control/static'),
 )
 
 
